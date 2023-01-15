5b4859ab40d4c14a1501ddc9170e47ade8baef8f
Olavo e Souto <olavo_souto@hotmail.com>
2023-01-15 19:50:25 -0300

    feat: Header, Icons import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,10 +9,9 @@ const Home: NextPage = () => {
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Lets build NetFlix
-
-      {/*header*/}
       
+      <Header/>
+
       <main>
       {/*BANNER*/}
         <section>
