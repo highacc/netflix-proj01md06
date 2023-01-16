78f7ce4a51b42cbb8b1078f327f6adf1c1a2e2dc
Olavo e Souto <olavo_souto@hotmail.com>
2023-01-16 09:56:50 -0300

    feat: Requests.ts and Banner structure

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Banner
