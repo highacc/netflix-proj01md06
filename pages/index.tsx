74a0adeed43c9752b1be74556eaa5cbfd85337ad
Olavo e Souto <olavo_souto@hotmail.com>
2023-01-26 22:14:13 -0300

    quick change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import type { NextPage } from 'next'
+import { getProducts, Product } from '@stripe/firestore-stripe-payments'
 import Head from 'next/head'
+import { useRecoilValue } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
+import Plans from '../components/Plans'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
+import useList from '../hooks/useList'
+import useSubscription from '../hooks/useSubscription'
+import payments from '../lib/stripe'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 import axios from 'axios'
-import useAuth from '../hooks/useAuth'
-import { useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
-import Modal from '../components/Modal'
-import Plans from '../components/Plans'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -21,6 +24,7 @@ interface Props {
   horrorMovies: Movie[]
   romanceMovies: Movie[]
   documentaries: Movie[]
+  products: Product[]
 }
 
 const Home = ({
@@ -32,34 +36,43 @@ const Home = ({
   romanceMovies,
   topRated,
   trendingNow,
+  products,
 }: Props) => {
-
   const { user, loading } = useAuth()
+  const subscription = useSubscription(user)
   const showModal = useRecoilValue(modalState)
-  const subscription = false
+  const movie = useRecoilValue(movieState)
+  const list = useList(user?.uid)
 
   if (loading || subscription === null) return null
 
-  if (!subscription) return <Plans/>
-  
+  if (!subscription) return <Plans products={products} />
+
   return (
-    <div  className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
-      showModal && '!h-screen overflow-hidden'
-    }`}>
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
-        <title>Home - Netflix</title>
+        <title>
+          {movie?.title || movie?.original_name || 'Home'} - Netflix
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <Header/>
+
+      <Header />
 
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
-      <Banner netflixOriginals={netflixOriginals} />
+        <Banner netflixOriginals={netflixOriginals} />
+
         <section className="md:space-y-24">
-        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
@@ -74,7 +87,12 @@ const Home = ({
 export default Home
 
 export const getServerSideProps = async () => {
-
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message))
 /*
   Requests using fetch api
 
@@ -131,6 +149,7 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
+      products,
     },
   }
 }
